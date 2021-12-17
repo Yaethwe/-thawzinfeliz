fea14dae4cdbc2bc49f1ee7f544d3cb18ff141d1
Yeaethwe <76844276+Yaethwe@users.noreply.github.com>
2021-12-17 21:42:56 +0630

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function closeShop() {
 	document.getElementById("shopDiv").className="hide";
 }
@@ -7,13 +6,25 @@ function closeAboutUs() {
 	document.getElementById("aboutusDiv").className="hide";
 }
 
+function closeViewLogo() {
+	document.getElementById("logoViewer").className="hide";
+}
+
 
 function aboutUs() {
 	document.getElementById("aboutusDiv").className="aboutusDiv";
 	closeShop();
+	closeViewLogo();
 }
 
 function shop() {
 	document.getElementById("shopDiv").className="shopDiv";
 	closeAboutUs();
+	closeViewLogo();
+}
+
+function viewLogo() {
+	document.getElementById("logoViewer").className="logoViewer";
+	closeAboutUs();
+	closeShop();
 }
